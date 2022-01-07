7e2c5f20bcc8b7d6a97a072e2cf8c2f9ae75a64f
Ruzina Biktasheva <96714017+RuzinaBiktasheva@users.noreply.github.com>
2022-01-07 18:03:49 +0300

    create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    let number = String(n);
+    let newN = '';
+    let i = number.length - 1;
+
+    if (number[0] != '-') {
+        while (i >= 0) {
+            newN += number[i];
+            i = i - 1;
+        }
+    } else {
+        while (i >= 1) {
+            newN += number[i];
+            i = i - 1;
+        }
+    } return newN;
 }
